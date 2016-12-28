b92547fca8b043bcc8adc69e806662a8725fae57
finder shahaf <finders@cs004row1-4.cs.bgu.ac.il>
2016-12-28 17:55:25 +0200

    updated Simulator

--- src/main/java/bgu/spl/a2/sim/Simulator.java
+++ src/main/java/bgu/spl/a2/sim/Simulator.java
@@ -5,29 +5,32 @@
  */
 package bgu.spl.a2.sim;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import bgu.spl.a2.WorkStealingThreadPool;
+
 
 /**
  * A class describing the simulator for part 2 of the assignment
  */
 public class Simulator {
 	/**
-	 * Begin the simulation Should not be called before
-	 * attachWorkStealingThreadPool()
-	 */
-	public static void start() {
-		// TODO
-	}
-
+	* Begin the simulation
+	* Should not be called before attachWorkStealingThreadPool()
+	*/
+    public static ConcurrentLinkedQueue<Product> start(){
+    	return null; // TODO
+    }
+	
 	/**
-	 * attach a WorkStealingThreadPool to the Simulator, this
-	 * WorkStealingThreadPool will be used to run the simulation
-	 * 
-	 * @param myWorkStealingThreadPool
-	 *            - the WorkStealingThreadPool which will be used by the
-	 *            simulator
-	 */
-	public static void attachWorkStealingThreadPool(WorkStealingThreadPool myWorkStealingThreadPool) {
-		// TODO
+	* attach a WorkStealingThreadPool to the Simulator, this WorkStealingThreadPool will be used to run the simulation
+	* @param myWorkStealingThreadPool - the WorkStealingThreadPool which will be used by the simulator
+	*/
+	public static void attachWorkStealingThreadPool(WorkStealingThreadPool myWorkStealingThreadPool){
+		
+	}
+	
+	public static int main(String [] args){
+		return 0;
 	}
 }
